6d829f09f06747949819e45378ac3ae31328f7ea
Wes <wfa207@gmail.com>
2016-05-24 12:50:47 -0400

    set up login authentication route; does NOT have put requests wired up to buttons

--- server/app/index.js
+++ server/app/index.js
@@ -2,7 +2,36 @@
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
+var User = require('../api/users/user.model')
 
+app.use(session({
+	// the following ensures that session IDs are not predictable
+	secret: 'tongiscool' // WHAT DOES THIS DO???
+}));
+
+app.use(function(req, res, next) {
+	console.log('session: ', req.session);
+	next();
+});
+
+app.post('/login', function(req, res, next) {
+	User.findOne({
+		where: req.body
+	})
+	.then(function(user) {
+		console.log(user);
+		if (!user) {
+			res.sendStatus(401);
+		} else {
+			req.session.userId = user.id;
+			res.sendStatus(204);
+		}
+	})
+	.catch(next);
+});
+
+// Why do we not call 'next()' in the routes below?
 app.use(require('./logging.middleware'));
 
 app.use(require('./request-state.middleware'));
